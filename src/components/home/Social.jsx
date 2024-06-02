57b158b5da39cf406d50264edb1cabe7ff4a3b06
Lakshya Dhawan <lakshyad1902@gmail.com>
2024-06-02 17:22:23 +0530

    added home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/yolakshya19"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/lakshya-dhawan19"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://leetcode.com/u/lakshyad1902"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-code-branch"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
